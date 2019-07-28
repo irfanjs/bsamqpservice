38249dc5f6304dc8b4a748914f7956ed6d08a411
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-28 18:37:24 +0530

    first commit

---
+++ src/main/java/com/infy/ci/bsamqpservice/BsamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.bsamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsamqpserviceApplication.class, args);
+	}
+
+}
